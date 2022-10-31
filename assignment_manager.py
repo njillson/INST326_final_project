fdefd85d84bc1450e20e2b6b19362c55a9071c8d
Natalie Jillson <njillson@terpmail.umd.edu>
2022-10-31 19:35:25 -0400

    added assignment_manager.py

---
+++ assignment_manager.py
@@ -0,0 +1,11 @@
+""" A tool for managing school assignments"""
+
+class Assignment:
+    """An assignment object
+    
+    Attributes:
+        name(str): name of assignment
+        course(str): course code
+        date(str): date assignment is due
+        time(str): time assignment is due
+    """
