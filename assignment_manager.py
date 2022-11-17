cdb130833e7e69ec1c0e61ffbcd8f8978c299b0e
seliuna <112517392+seliuna@users.noreply.github.com>
2022-11-16 22:06:04 -0500

    Update assignment_manager.py

--- assignment_manager.py
+++ assignment_manager.py
@@ -9,12 +9,9 @@ class Assignment:
         date(str): date assignment is due
         time(str): time assignment is due
     """
-    def sort_due_dates():
-        """A method that sorts through the due dates from most important to least important 
-        """ 
-    def add_to_callender():
-        """A meathod that adds the due date to another file called callender""" 
-    def sort_assingment_type():
-        """A method that sorts assignments based on which class its for"""     
-    def class_names():
-       """A method that sorts classes based on class name"""
+    def sort_assingment():
+        """A method that sorts assignments based on which class its for
+        Custom list sorting with a key function with 2 criteria, first by date, then by point value"""     
+
+    def __add__():
+        """magic method that adds points for a class for the week"""
