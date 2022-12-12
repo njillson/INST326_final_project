017b12c130b70383aa95e52f6600cf5bdb5491f3
Selina Liu <sliu1235@umd.edu>
2022-12-11 22:38:10 -0500

    forgot to remove hashtags

--- assignment_manager.py
+++ assignment_manager.py
@@ -5,8 +5,8 @@ import sys
 from datetime import datetime
 import re
 import pandas as pd
-#import seaborn as sns
-#import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.pyplot as plt
 
  
 class Assignment:
